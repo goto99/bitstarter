54177266942d793954076337709d63fa05912e7d
Tom Kurt <goto99@gmail.com>
2013-07-17 06:35:14 +0000

    updated grader.js

--- grader.js
+++ grader.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development and basic DOM parsing.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
 
 References:
 
@@ -23,11 +24,24 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
+var urlHandler = function(infile) {
+    var instr = infile.toString();
+    rest.get(instr).on('complete', function(result) {
+        if (result instanceof Error) {
+	    console.log("%s does not exist. Exiting.", instr);
+            process.exit(1);
+         } else {
+            return result;
+        }
+};
+
 var assertFileExists = function(infile) {
     var instr = infile.toString();
+
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
@@ -64,8 +78,8 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <http_url>', 'External url to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-	.parse(process.argv);
+	.option('-u, --url <http_url>', 'External url to index.html', urlHandler, HTMLFILE_DEFAULT)
+        .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
